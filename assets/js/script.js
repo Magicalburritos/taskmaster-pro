bf0aa176e935938b7c2fb8378af921f72b84d3d2
joe <hudakjoe@msn.com>
2021-12-03 18:20:01 -0500

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,15 +45,16 @@ var saveTasks = function () {
 };
 
 var auditTask = function (taskEl) {
+  console.log(taskEl);
+
   // get date from task element
+
   var date = $(taskEl).find('span').text().trim();
 
   console.log(date);
 
   // convert to moment object at 5:00pm
   var time = moment(date, 'L').set('hour', 17);
-
-  console.log(time);
 
   // remove any old classes from element
   $(taskEl).removeClass('list-group-item-warning list-group-item-danger');
@@ -74,16 +75,18 @@ $('.card .list-group').sortable({
   tolerance: 'pointer',
   helper: 'clone',
   activate: function (event, ui) {
-    console.log(ui);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function (event, ui) {
-    console.log(ui);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function (event) {
-    console.log(event);
+    $(event.target).addClass('dropover-active');
   },
   out: function (event) {
-    console.log(event);
+    $(event.target).removeClass('dropover-active');
   },
   update: function () {
     var tempArr = [];
@@ -106,9 +109,6 @@ $('.card .list-group').sortable({
     tasks[arrName] = tempArr;
     saveTasks();
   },
-  stop: function (event) {
-    $(this).removeClass('dropover');
-  },
 });
 
 // trash icon can be dropped onto
@@ -118,12 +118,14 @@ $('#trash').droppable({
   drop: function (event, ui) {
     // remove dragged element from the dom
     ui.draggable.remove();
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   over: function (event, ui) {
     console.log(ui);
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function (event, ui) {
-    console.log(ui);
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
 });
 
@@ -146,7 +148,7 @@ $('#task-form-modal').on('shown.bs.modal', function () {
 });
 
 // save button in modal was clicked
-$('#task-form-modal .btn-primary').click(function () {
+$('#task-form-modal .btn-save').click(function () {
   // get form values
   var taskText = $('#modalTaskDescription').val();
   var taskDate = $('#modalDueDate').val();
@@ -252,6 +254,11 @@ $('#remove-tasks').on('click', function () {
   console.log(tasks);
   saveTasks();
 });
+setInterval(function () {
+  $('.card .list-group-item').each(function (index, el) {
+    auditTask(el);
+  });
+}, 5000);
 
 // load tasks for the first time
 loadTasks();
